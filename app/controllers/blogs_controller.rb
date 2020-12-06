4a331b5ac6c08edc441d3b9d7433340a245cbc3d
scarter31191 <scarter31191@gmaile.com>
2020-12-06 10:25:07 -0500

    built blog show

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,4 +4,40 @@ class BlogsController < ApplicationController
         @blog = Blog.new
     end
 
+    def create
+        @blog = Blog.new(blog_params)
+        if @blog.save
+            redirect_to blogs_show_path(@blog)
+        else
+            redirect_to blogs_new_path
+        end
+    end
+
+
+
+    def index
+        @blogs = Blog.all
+    end
+
+    def show
+        @blog = Blog.find(params[:id])
+    end
+
+
+
+
+    
+
+
+
+
+
+
+
+
+    private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-
+  get '/blogs', to: 'blogs#index'
   get '/blogs/new', to: 'blogs#new'
   post '/blogs', to: 'blogs#create'
+  get '/blogs/:id', to: 'blogs#show', as: 'blogs_show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
